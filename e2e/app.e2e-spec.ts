dd61cfef433a973a01dcb28d59a7cc81c6678776
Eric Lopes Pedroso <elpedroso@voegol.com.br>
2019-09-30 22:48:58 -0300

    Ajustando arquivos do projeto para versao anterior devido erro de install pacote

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('workspace-project App', () => {
+describe('app1 App', () => {
   let page: AppPage;
 
   beforeEach(() => {
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to angular-site-aplication-one!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
