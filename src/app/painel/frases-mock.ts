787128c4686377045615e660e4d08914d01d0957
Eric Lopes Pedroso <elpedroso@voegol.com.br>
2019-10-03 23:54:40 -0300

    Criado mock de frases para a aplicacao

---
+++ src/app/painel/frases-mock.ts
@@ -0,0 +1,8 @@
+import { Frase } from '../shared/frase.model'
+
+const FRASES: Frase[] = [
+    { fraseEng: 'I Like to Learn', frasePtBr: 'Eu gosto de aprender' },
+    { fraseEng: 'I watch tv', frasePtBr: 'Eu assisto tv' },
+    { fraseEng: 'How are you?', frasePtBr: 'Como vai voce' },
+    { fraseEng: 'I eat bread', frasePtBr: 'Eu como pao' }
+]
